44aa6b184cb3036d428a710b49c0c367f12a7390
tuza john <johntuza94@hotmail.com>
2022-03-08 13:03:01 +0300

    Add project files.

---
+++ TechInterView.Fibonacci/FibNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace TechInterView.Fibonacci;
+
+public class FibNumber
+{
+    private static int firstNumber = 0, secondNumber = 1;
+
+    public static int GetFib(int position)
+    {
+        if (position > 1)
+        {
+            for (int i = 1; i < position; i++)
+            {
+                //current sum
+                var fibonacciNumber = secondNumber + firstNumber; 
+
+                firstNumber = secondNumber;
+
+
+                //second number variable becomes the fibonacci number
+                secondNumber = fibonacciNumber;
+            }
+
+            //basic substitution to maintain same variable and accomodatin for when position is either 1 or 2
+            position = secondNumber;
+        }
+
+        return position;
+    }
+}
---
+++ TechInterView.Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿using TechInterView.Fibonacci;
+
+A:
+Console.WriteLine("Enter number that will reprsent the position in the fibonacci: ");
+var input = Console.ReadLine();
+
+if (!int.TryParse(input, out int position)) 
+{
+    Console.WriteLine("Insert valid number");
+    goto A;
+}
+
+//Ensure absolut value
+position = position < 0 ? position * -1 : position;
+
+var result = FibNumber.GetFib(position);
+
+Console.WriteLine($"Fibonacci number at position: {position} is {result}");
